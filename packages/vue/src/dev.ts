eeb3af6fff8e702c18a4079ce15a0b94c829b9b4
542154968 <542154968@qq.com>
2020-08-17 11:06:34 +0800

    feat(packages): devtools hook的初始化 浏览器环境消息提示

--- packages/vue/src/dev.ts
+++ packages/vue/src/dev.ts
@@ -5,9 +5,12 @@ export function initDev() {
   // 获取全局的this
   const target = getGlobalThis()
 
+  // 全局注入__VUE__ flag
   target.__VUE__ = true
+  // 设置devtools的hook对象 用来相互调用
   setDevtoolsHook(target.__VUE_DEVTOOLS_GLOBAL_HOOK__)
 
+  // 如果是浏览器环境 提示信息
   if (__BROWSER__) {
     console.info(
       `You are running a development build of Vue.\n` +
