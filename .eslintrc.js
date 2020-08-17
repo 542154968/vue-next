636dbc9905bafb11a906968cb09fa3c9ddf891f0
542154968 <542154968@qq.com>
2020-08-17 09:18:08 +0800

    feat(eslint): 规则的一些注释

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,18 +7,24 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    // 该规则旨在消除未使用的变量，函数和函数的参数。
     'no-unused-vars': [
       'error',
       // we are only using this rule to check for unused arguments since TS
       // catches unused variables but not args.
       { varsIgnorePattern: '.*', args: 'after-used', argsIgnorePattern: '^_' }
     ],
+    // 该规则允许您指定您不希望在应用程序中使用的全局变量名称。
     // most of the codebase are expected to be env agnostic
     'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
     // since we target ES2015 for baseline support, we need to forbid object
+    // 由于我们将ES2015作为基线支持的目标，我们需要禁止object
     // rest spread usage (both assign and destructure)
+    // rest spread用法（assign和destructure）
+    // 此规则不允许指定（即用户定义）语法。
     'no-restricted-syntax': [
       'error',
+      // https://m.imooc.com/wenda/detail/562808 SpreadElement
       'ObjectExpression > SpreadElement',
       'ObjectPattern > RestElement'
     ]
